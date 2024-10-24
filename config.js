a91aa11ecae70b4ce6361a0e5364107bef6db952
Jupiter Bold <sanadababy5@gmail.com>
2024-10-24 07:47:57 +0100

    Pʟᴀᴛɪɴᴜᴍ-V1

--- config.js
+++ config.js
@@ -45,7 +45,7 @@ global.SESSION_ID =
   ""
 module.exports = {
   menu: process.env.MENU || "2",
-  HANDLERS: process.env.PREFIX || "?",
+  HANDLERS: process.env.PREFIX || "+",
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
   caption: process.env.CAPTION || "Ͳհҽ օղҽ ąҍօѵҽ ąӀӀ ☠️👑🌍™",
